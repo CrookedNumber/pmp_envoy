9b07ae414c59ba9cff0a2cf68f01f42202667adc
Kathleen Grant <kgrant@npr.org>
2016-10-11 15:52:10 -0400

    US8502: Create vagrant profile

---
+++ credentials.php
@@ -0,0 +1,13 @@
+<?php
+
+$creds['sandbox'] = array(
+  'host' => 'https://api-sandbox.pmp.io',
+  'client_id' => "e38e15de-d164-4e96-a617-5f7d39ab743f",
+  'client_secret' => "5c4459ddf82aeb5551cf239a",
+);
+
+$creds['production'] = array(
+  'host' => 'https://api.pmp.io',
+  'client_id' => "<redacted>",
+  'client_secret' => "<redacted>",
+);
